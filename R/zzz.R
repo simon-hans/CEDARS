b1fbe8353a4c1875e78c53a756dd30b132afb34a
Simon Mantha <smantha@cedars.io>
2021-01-11 20:09:55 -0500

    cedars.env fixed in zzz

--- R/zzz.R
+++ R/zzz.R
@@ -11,7 +11,7 @@
 
     op <- options(scipen = 999)
 
-    cedars.env <- new.env(parent=emptyenv())
+    cedars.env <<- new.env(parent=emptyenv())
 
     utils::globalVariables(c("CUI1", "CUI2", "LAT", "SAB", "col_character", "cols", "doc_id", "end", "lemma", "negated", "negex", "negex_category", "nlp_model",
         "paragraph_id", "reviewed", "sentence_id", "start.x", "start.y", "text_date", "text_id", "umls_CUI", "umls_end",
