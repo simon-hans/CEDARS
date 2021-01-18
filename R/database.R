836731cd883213f06fb9cbbee1b9e48b6fbdf492
Simon Mantha <smantha@cedars.io>
2021-01-18 16:14:16 -0500

    Updated main example

--- R/database.R
+++ R/database.R
@@ -701,9 +701,22 @@ delete_end_user <- function(uri_fun, user, password, host, port, database, end_u
 #' automatic_NLP_processor(NA, "latin1", "udpipe", uri_fun, db_user_name, db_user_pw,
 #' db_host, db_port, mongo_database, max_n_grams_length = 0, negex_depth = 6, select_cores = 1)
 #'
-#' # Starts the CEDARS GUI locally
+#' # Pre-searching based on query
+#' # This is optional but will speed-up the interface
+#' pre_search(patient_vect = NA, uri_fun, db_user_name, db_user_pw, db_host, db_port, mongo_database)
+#'
+#' # Start the CEDARS GUI locally
 #' # Your user name is "John", password is "strongpassword"
+#' # Once you have entered those credentials, click on button "ENTER NEW DATE" and CEDARS will
+#' # seek the first record to annotate
+#' # Try out the interface, adjudicating sentences, entering event dates, comments, moving
+#' # between sentences and searching for records
+#' # Once you have entered some data, close the GUI
 #' start_local(db_user_name, db_user_pw, db_host, db_port, mongo_database)
+#'
+#' # Obtaining events and info associated with data entry
+#' # The annotations entered in the GUI are now available in this dataframe
+#' event_output <- download_events(uri_fun, db_user_name, db_user_pw, db_host, db_port, mongo_database)
 #' }
 #'
 #' # Remove project from MongoDB
