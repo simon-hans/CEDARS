80e1053b4888a95cfcaf3921e577669fd7015ceb
Simon Mantha <smantha@cedars.io>
2021-01-17 19:33:31 -0500

    Minor changes to manual

--- R/database.R
+++ R/database.R
@@ -679,8 +679,8 @@ delete_end_user <- function(uri_fun, user, password, host, port, database, end_u
 #'
 #' # Negex is included with CEDARS and required for assessment of negation
 #' negex_upload(uri_fun, db_user_name, db_user_pw, db_host, db_port, mongo_database)
-#'
 #' }
+#'
 #' # Uploading the small simulated collection of EHR corpora
 #' upload_notes(uri_fun, db_user_name, db_user_pw, db_host, db_port, mongo_database,
 #' simulated_patients)
@@ -704,8 +704,8 @@ delete_end_user <- function(uri_fun, user, password, host, port, database, end_u
 #' # Starts the CEDARS GUI locally
 #' # Your user name is "John", password is "strongpassword"
 #' start_local(db_user_name, db_user_pw, db_host, db_port, mongo_database)
-#'
 #' }
+#'
 #' # Remove project from MongoDB
 #' terminate_project(uri_fun, db_user_name, db_user_pw, db_host, db_port, mongo_database, fast=TRUE)
 #' @export
