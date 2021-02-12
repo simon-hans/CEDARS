c26dafc45ea6aeb92c95cb17111716bd70a96e83
Simon Mantha <smantha@cedars.io>
2021-02-12 15:08:01 -0500

    Update text_processing.R
    Search at level of notes rather than text fragment for auto NLP processor.

--- R/text_processing.R
+++ R/text_processing.R
@@ -322,17 +322,17 @@ automatic_NLP_processor <- function(patient_vect = NA, text_format = "latin1", n
 
         print(paste("checking patient #", i, "of", l_all_patients))
 
-        # All text_id's available for annotation
-        all_notes <- notes_con$find(query = paste("{\"patient_id\" :", all_patients[i] , "}"), fields = "{ \"text_id\" : 1, \"_id\" : 0 }")
+        # All note_id's available for annotation
+        all_notes <- notes_con$find(query = paste("{\"note_id\" :", all_patients[i] , "}"), fields = "{ \"note_id\" : 1, \"_id\" : 0 }")
 
         # This will have to be removed once we convert everything to numeric!
-        all_notes <- trimws(all_notes$text_id)
+        all_notes <- trimws(all_notes$note_id)
 
-        # All text_id's with an existing annotation
-        all_annotated <- annotations_con$find(query = paste("{\"patient_id\" :", all_patients[i] , "}"), fields = "{ \"text_id\" : 1, \"_id\" : 0 }")
+        # All note_id's with an existing annotation
+        all_annotated <- annotations_con$find(query = paste("{\"note_id\" :", all_patients[i] , "}"), fields = "{ \"note_id\" : 1, \"_id\" : 0 }")
 
         # This will have to be removed once we convert everything to numeric!
-        all_annotated <- trimws(unique(all_annotated$text_id))
+        all_annotated <- trimws(unique(all_annotated$note_id))
 
         missing_text <- all_notes[!(all_notes %in% all_annotated)]
 
